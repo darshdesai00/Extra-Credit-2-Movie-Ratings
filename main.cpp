8dee9482e97fb933197008e61e2d35560fff56bc
darshdesai00 <darshdesai50@icloud.com>
2025-12-09 17:36:04 -0800

    Working on Milestone 2 avg

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
 
-    map<string, vector<int>> movies;
+    map<string, vector<int> > movies;
     ifstream file("210-xc2-FA25.txt");
 
     // Milestone 1
@@ -25,6 +25,21 @@ int main() {
         cout << endl;
     }
 
+    
+    // Milestone 2
+    cout << "\n Milestone 2: Averages \n";
+
+    map<string, double> averages;
+    int totalMovies = 0;
+
+    for (auto &p : movies) {
+        int sum = 0;
+        for (int r : p.second) sum += r;
+
+    double avg = (double)sum / p.second.size();
+        averages[p.first] = avg;
+    
+    
     return 0;
 }
 
