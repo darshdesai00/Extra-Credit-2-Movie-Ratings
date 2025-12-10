a7cd7121af5453be78297a593f3049a7cc7e248a
darshdesai00 <darshdesai50@icloud.com>
2025-12-09 17:23:39 -0800

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -10,5 +10,23 @@ int main() {
     map<string, vector<int>> movies;
     ifstream file("210-xc2-FA25.txt");
 
+    // Milestone 1
+    string title;
+    int rating;
+
+    while(file >> title >> rating) {
+        movies[title].push_back(rating);    
+    }
+    
+    cout << "\n Milestone 1: Movies AND Ratings \n";
+    for (auto &p : movies) {
+        cout << p.first << ": ";
+        for (int r : p.second) cout << r << " ";
+        cout << endl;
+    }
+
     return 0;
 }
+
+
+
