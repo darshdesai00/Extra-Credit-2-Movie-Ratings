c4fbc552ca534407b3c5240a5e3d6bc903d3b2ad
darshdesai00 <darshdesai50@icloud.com>
2025-12-09 17:03:31 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+
+    map<string, vector<int>> movies;
+    ifstream file("210-xc2-FA25.txt");
+
+    return 0;
+}
